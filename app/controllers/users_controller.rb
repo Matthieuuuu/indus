e95a34fe2bcb67948b9912f6a3316acba8e22c22
magiknono <arno.cormier@gmail.com>
2014-12-02 15:52:55 +0100

    added users#show and edit controllers and view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,12 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+    @user.create(user_params)
+    redirect_to user_path(@user)
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :description, :phone, :birthday)
   end
 end
