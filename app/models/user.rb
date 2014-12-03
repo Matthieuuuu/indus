caf6f7369fef78dbcfd58ebf25cf9f8e6569d2cb
magiknono <arno.cormier@gmail.com>
2014-12-03 15:02:43 +0100

    1 migration + comments the paperclip view,controller and model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,11 @@ class User < ActiveRecord::Base
   has_many :stays
   has_many :sites
 
-  has_attached_file :picture,
-    styles: { medium: "300x300>", thumb: "100x100>" }
+  # has_attached_file :picture,
+  #   styles: { medium: "300x300>", thumb: "100x100>" }
 
-  validates_attachment_content_type :picture,
-    content_type: /\Aimage\/.*\z/
+  # validates_attachment_content_type :picture,
+  #   content_type: /\Aimage\/.*\z/
 
   def self.find_for_facebook_oauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
