2438895d233b60587d6655ce4225ce1d5d74ecfa
Matthieu Corcin <matthieu.corcin@gmail.com>
2014-12-04 00:54:12 +0100

    calendar

--- app/controllers/stays_controller.rb
+++ app/controllers/stays_controller.rb
@@ -3,22 +3,33 @@ class StaysController < ApplicationController
     @stay = Stay.new
   end
 
-  def create
-    stay = Stay.create(user: current_user)
-    stay.update(stay_params)
-    redirect_to stays_path
-  end
-
   def update
     stay = Stay.find(params[:id])
     stay.update(stay_params) unless stay.status == "cancelled"
     redirect_to stays_path
   end
 
+  def create
+    stay = Stay.create(user: current_user)
+    end_date = process_date(params[:end_date])
+    begin_date = process_date(params[:begin_date])
+    stay.update({site_id: params[:stay][:site_id], end_date: end_date, begin_date: begin_date})
+    raise unless (stay.begin_date || stay.end_date)
+    redirect_to stays_path
+  end
+
   private
 
   def stay_params
-    params.require(:stay).permit(:site_id, :begin_date, :end_date, :status, :response_at)
+    params.require(:stay).permit(:site_id, :status, :response_at, :begin_date, :end_date)
+  end
+
+  def process_date(date)
+    date_array = date.split("/")
+    day = date_array[1]
+    month = date_array[0]
+    year = date_array[2]
+    day + "/" + month + "/" + year
   end
 
 end
